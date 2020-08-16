fb2e968c04bc4cf55801f0ac59421cca8e0f6fdd
Hyung Kim <hjkmines@gmail.com>
2020-08-16 16:06:40 -0600

    adds login comp

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,11 +8,7 @@ function renderConditionally() {
     return <h1>Hello</h1>
   } else {
     return (
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+      
     )
   }
 }
