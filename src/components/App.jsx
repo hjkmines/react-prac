67a39432b99d840163005a5dd2429ed8611f15f1
Hyung Kim <hjkmines@gmail.com>
2020-08-16 17:05:11 -0600

    adds hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 function App() {
-  return <div />;
+
+  const [count, setCount] = useState(21); 
+  
+  function inc() {
+      setCount(12)
+  }
+  
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={inc} >+</button>
+    </div>
+  )
 }
+
+
 
 export default App;
