bd90bb87079e1d42e2e134349c5cde5fc0299dc8
Hyung Kim <hjkmines@gmail.com>
2020-08-16 15:36:59 -0600

    adds cond render

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import notes from '../notes'
-import Footer from './Footer'
-import Header from './Header'
-import Note from './Note'
+import React from 'react'; 
+import { render } from '@testing-library/react';
+
+let isLoggedIn = false; 
+
+function renderConditionally() {
+  if(isLoggedIn === true) {
+    return <h1>Hello</h1>
+  } else {
+    return (
+      <form className="form">
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    )
+  }
+}
 
 function App() {
   return (
-    <div>
-      <Header />
-      {notes.map( note => 
-       <Note 
-         key={note.key}
-         title={note.title}
-         content={note.content}
-       />
-       )}
-      <Footer />
+    <div className="container">
+      {renderConditionally()}
     </div>
-  )
+  );
 }
 
 export default App 
