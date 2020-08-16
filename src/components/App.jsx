0528e8825538d601b3ea5a2f01324978da8b14c7
Hyung Kim <hjkmines@gmail.com>
2020-08-16 16:51:08 -0600

    adds button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,7 @@
-import React from 'react'; 
-import Login from './Login'
-
-let isLoggedIn = false; 
-
-function renderConditionally() {
-  if(isLoggedIn === true) {
-    return <h1>Hello</h1>
-  } else {
-    return (
-      
-    )
-  }
-}
+import React from "react";
 
 function App() {
-  return (
-    <div className="container">
-      {renderConditionally()}
-    </div>
-  );
+  return <div />;
 }
 
-export default App 
+export default App;
